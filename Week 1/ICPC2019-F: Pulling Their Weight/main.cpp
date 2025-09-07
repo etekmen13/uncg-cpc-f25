fbfc5ab1ead7eb3d29f8ffe68f574d8f898d485e
etekmen13 <etekmen13@gmail.com>
2025-09-07 16:39:09 -0400

    Complete ICPC 2019 F: Pulling Their Weight

--- Week 1/ICPC2019-F: Pulling Their Weight/main.cpp
+++ Week 1/ICPC2019-F: Pulling Their Weight/main.cpp
@@ -54,9 +54,64 @@ ostream& operator <<(ostream& o, const vector<T>& vec) {
 
 #pragma endregion
 
+#pragma region Answer
+void answer() {
+    /*
+    Objective: Find smallest t that splits the set equally by sum.
+
+    Note: Some weights W will be == t. If |W| is even, split W evenly.
+    else, remove one and then split W evenly.
+    
+    Note: There will *always* exist a t that works.
+    Example:
+    1 2 3 6. t = 4 -> {1,2,3} = {6}
+    3 8 10 11. t = 10 -> {3, 8} = {11} (10 is discarded)
+
+    Idea: Compute prefix sum forwards and compare to suffix sum.
+
+    3  8  9 11
+    Total: 31
+    3 11 |20 31
+    29 20 11 0
+    
+    0  3  11 20
+    29 20 11  0
+
+    Case 1: t is not in the set (prefix == suffix). return t + 1
+    Case 2: t is in the set even # times (prefix == suffix). return t.
+        - Check input bounds. smallest case is {1,1}. We can safely check if v[i] == v[i+1] to determine if we are in this case.
+    Case 3: t is in the set odd # times (prefix < suffix). return t.
+    */
+   int m;
+   cin >> m;
+   vector<int> vec(m);
+   rep(i, 0, m) {
+    cin >> vec[i];
+   }
+   sort(vec.begin(), vec.end());
+   ll total = accumulate(vec.begin(), vec.end(), 0);
+   ll sum = 0;
+   rep(i, 0, m) {
+    if (sum + vec[i] == total - sum - vec[i]) {
+
+        cout << vec[i] + (vec[i] != vec[i+1]) << "\n";
+        return;
+    }
+    if (sum + vec[i] > total - sum - vec[i]) {
+        cout << vec[i] << "\n";
+        return;
+    }
+    sum += vec[i];
+   }
+}
+#pragma endregion
 void solve() {
    
 }
+
+// Problem Link:
+// https://open.kattis.com/problems/pullingtheirweight
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
